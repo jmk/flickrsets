60e03c73e428f1ec5d09c0dc76afa9dc5d6cd7a7
Jason Kim <jason@512k.org>
2011-05-26 03:50:48 -0700

    Increased appstats profiler stack depth.

--- appengine_config.py
+++ appengine_config.py
@@ -2,6 +2,11 @@
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 
+appstats_MAX_STACK = 15
+appstats_MAX_LOCALS = 15
+appstats_MAX_REPR = 100
+appstats_MAX_DEPTH = 15
+
 def webapp_add_wsgi_middleware(app):
    from google.appengine.ext.appstats import recording
    app = recording.appstats_wsgi_middleware(app)
